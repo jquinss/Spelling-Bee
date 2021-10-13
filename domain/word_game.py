bba8b70b22058b17adb90d7a3503e9c4994616d5
Joaquin Sampedro <jquinss@yahoo.es>
2021-10-13 15:17:01 +0100

    Added class SpellingBeeGame

--- domain/word_game.py
+++ domain/word_game.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from services.lookup_service import LookupService
 
 class WordGameTemplate(ABC):
     def __init__(self, word_lookup_service):
@@ -16,8 +17,12 @@ class WordGameTemplate(ABC):
     def evaluate_word(self, word):
         pass
 
+    @abstractmethod
     def lookup_entry(self, word, **kwargs):
-        if self.word_lookup_service.lookup_entry(word, **kwargs):
+        pass
+
+    def is_in_dictionary(self, word, dictionary_index):
+        if self.word_lookup_service.lookup_entry(dictionary_index, word) is not None:
             return True
         return False
 
@@ -28,3 +33,58 @@ class WordGameTemplate(ABC):
             score = self.calculate_score(word)
             self.total_scores[self.current_player_index] += score
         return score, self.total_scores[self.current_player_index], message
+
+
+class SpellingBeeGame(WordGameTemplate):
+    MIN_WORD_LEN = 4
+    PANGRAM_LEN = 7
+    PANGRAM_BONUS = 7
+    REQUIRED_LETTER_INDEX = 3
+
+    def __init__(self, word_lookup_service):
+        super().__init__(word_lookup_service)
+        self.words_found_list = []
+        self.pangram = self.get_random_pangram()
+
+    def get_random_pangram(self):
+        return self.word_lookup_service.get_random_entry_index("pangram_dict")
+
+    def randomize_word(self):
+        pass
+
+    def calculate_score(self, word):
+        word_len = len(word)
+        if word_len == self.MIN_WORD_LEN:
+            return 1
+        elif self.MIN_WORD_LEN <= word_len < self.PANGRAM_LEN:
+            return word_len
+        elif word_len >= 7:
+            return self.PANGRAM_LEN + self.PANGRAM_BONUS + (word_len - self.PANGRAM_LEN)
+        else:
+            return 0
+
+    def evaluate_word(self, word):
+        if word in self.words_found_list:
+            return False, "The word {} has already been found".format(word)
+
+        letters_set = set(word)
+        pangram_set = set(self.pangram)
+
+        if len(word) < 4 or not letters_set.issubset(pangram_set) or not self.is_in_dictionary(word, "word_dict"):
+            return False, "Sorry, that is not a valid word"
+
+        center_letter = self.pangram[self.REQUIRED_LETTER_INDEX]
+
+        if center_letter not in word:
+            return False, "Invalid word, missing centre letter {}".format(center_letter)
+
+        if letters_set.issubset(pangram_set):
+            if letters_set == self.PANGRAM_LEN:
+                return True, "Pangram!"
+            else:
+                return True, "Valid word"
+
+        return False, "Sorry that is not a valid word"
+
+
+
