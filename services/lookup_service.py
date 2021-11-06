5b42578e2759b00146631732f7f80bcb7cf5a8f7
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-11-06 14:17:37 +0000

    Added comments

--- services/lookup_service.py
+++ services/lookup_service.py
@@ -11,10 +11,17 @@ class LookupService(ABC):
             self.data_source = None
 
     @abstractmethod
-    def load_source_file(self, source_file):
+    def load_source_file(self, source_file): # loads data from a file to a dictionary
+        """Primitive operation. It needs to be overriden. Different implementations may exist, depending on the data
+        source format (e.g. JSON, XML, etc.)"""
         pass
 
     def lookup_entry(self, data_source_index, entry_index):
+        """
+            :param data_source_index: index of the dictionary
+            :param entry_index: index of the entry inside the dictionary
+            :return: dictionary entry or None (if no entry is found)
+        """
         if self.data_source is None:
             raise InvalidDataSourceException("No data source has been provided. Load a data source file first")
         if entry_index in self.data_source[data_source_index]:
@@ -22,13 +29,17 @@ class LookupService(ABC):
         return None
 
     def get_random_entry_index(self, data_source_index):
+        """
+            :param data_source_index: index of the dictionary
+            :return: random index inside the dictionary
+        """
         if self.data_source is None:
             raise InvalidDataSourceException("No data source has been provided. Load a data source file first")
         rand_num = randint(0, len(self.data_source[data_source_index]))
         random_index = list(self.data_source[data_source_index].keys())[rand_num]
         return random_index
 
-    def load_data_source(self, source_files_dict):
+    def load_data_source(self, source_files_dict): # loads data from multiples files to a dictionary of dictionaries
         data_source = {}
         for source_file_index in source_files_dict:
             data_source[source_file_index] = self.load_source_file(source_files_dict[source_file_index])
@@ -54,6 +65,7 @@ class LookupServiceFactory:
             return JSONLookupService(source_files_dict=source_files_dict)
         else:
             raise ValueError(lookup_type)
+
 
 class InvalidDataSourceException(Exception):
     pass
