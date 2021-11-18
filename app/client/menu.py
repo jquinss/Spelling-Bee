66fdb1c71cc61d0d7149f00ca2f832adfdcc772e
Joaquin Sampedro <j.sampedro-sarasquete@mycit.ie>
2021-11-18 12:40:31 +0000

    changed methods names

--- app/client/menu.py
+++ app/client/menu.py
@@ -20,18 +20,18 @@ class Menu(ABC):
         pass
 
 
-class ClientMenu(Menu):
+class GameMainMenu(Menu):
     def __init__(self):
         self.menu_text = "1. Create a new game\n" \
                             "2. Join an existing game\n" \
                             "3. Quit"
         super().__init__(self.menu_text)
-        self.options = {"1": self.start_game, "2": self.join_game, "3": self.exit_menu}
+        self.options = {"1": self.create_new_game, "2": self.join_existing_game, "3": self.exit_menu}
 
-    def start_game(self):
+    def create_new_game(self):
         print("Starting game")
 
-    def join_game(self):
+    def join_existing_game(self):
         print("Joining game")
 
     def process_selection(self, choice):
