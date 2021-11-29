5ef4c1e9350bafb7b33aa2b8bd9f2d593eb81acb
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-11-29 18:15:39 +0000

    modified class to be more general

--- services/stats_queue.py
+++ services/stats_queue.py
@@ -2,8 +2,8 @@ import pika
 import json
 
 
-class StatsQueue:
-    def __init__(self, hostname="localhost", queue_name="spellingbee"):
+class MessageQueueSender:
+    def __init__(self, queue_name, hostname="localhost"):
         self.hostname = hostname
         self.queue_name = queue_name
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.hostname))
@@ -13,8 +13,8 @@ class StatsQueue:
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.hostname))
         self.channel = self.connection.channel().queue_declare(self.queue_name)
 
-    def send(self, routing_key, stats_dict):
-        self.channel.basic_publish(exchange='', routing_key=routing_key, body=json.dumps(stats_dict))
+    def send(self, routing_key, message):
+        self.channel.basic_publish(exchange='', routing_key=routing_key, body=message)
 
     def close_connection(self):
         self.connection.close()
