a9403cca68c2a384f6052f50d13a29dfa2925bee
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-12-19 17:52:33 +0000

    Added comments

--- services/stats_processor.py
+++ services/stats_processor.py
@@ -9,8 +9,9 @@ class Observer(ABC):
         pass
 
 
+# class used to send the stats to the RabbitMQ queue
 class MessageQueueSender:
-    def __init__(self, queue_name, hostname="localhost"):
+    def __init__(self, queue_name, hostname="127.0.0.1"):
         self.hostname = hostname
         self.queue_name = queue_name
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.hostname))
@@ -29,10 +30,12 @@ class MessageQueueSender:
         self.connection.close()
 
 
+# implements the Observer abstract class. Will be updated by the WordGameServer whenever the user scores
 class GameStatsQueueProcessor(Observer):
     def __init__(self, queue_sender):
         self.queue_sender = queue_sender
 
     def update(self, server, game_id):
+        # calls the get_game_status method to get the stats from the observable (WordGameServer)
         game_status = server.get_game_status(game_id)
         self.queue_sender.send(json.dumps(game_status))
