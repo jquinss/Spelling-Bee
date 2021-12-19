0ac6a59fc607b398fa947f51c8e4bde59ba8f16b
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-12-19 14:41:31 +0000

    Added timestamp

--- app/client/stats_client.py
+++ app/client/stats_client.py
@@ -7,13 +7,13 @@ PORT = 65512
 
 def print_stats(stats):
     pangram = stats["pangram"]
-    print(f"Pangram: {pangram}")
-    print("Players:")
+    timestamp = stats["timestamp"]
+    print(f"Timestamp: {timestamp} - Pangram: {pangram} - ", end="")
     for player in stats["players"]:
         player_stats = stats["players"][player]
         total = player_stats["total"]
         words = player_stats["words"]
-        print(f"Username: {player}, Total score: {total}, Words found: {words},")
+        print(f"Player: {player}, Total score: {total}, Words found: {words} - ", end="")
     word_scores = stats["words_found"]
     print(f"Scores per word: {word_scores}")
 
